6575677be4dd02871f4aabd22678f2fe82869802
Julian Schallenmueller <29258884+julian2409@users.noreply.github.com>
2023-10-03 01:11:29 +0200

    Convert to async await syntax

--- src/components/CourseList.tsx
+++ src/components/CourseList.tsx
@@ -3,13 +3,14 @@ import axios from 'axios';
 function CourseList() {
   const url = "http://localhost:8080/v1/courses";
 
-  function getCourses() {
-    axios.get(url + "/")
-      .then(response => {
-        console.log(response.data);
-      }).catch(error => {
-        console.error(error);
-      });
+  const getCourses = async () => {
+    const res = await axios.get(url + "/");
+    try {
+      const data = res.data;
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
   }
   getCourses();
 
